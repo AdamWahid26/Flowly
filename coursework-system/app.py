a40e999476364ca27b59566722039b81c68bc4fc
KHALED <kaldinkhaled@gmail.com>
2026-04-16 15:14:06 +0800

    new feature

--- coursework-system/app.py
+++ coursework-system/app.py
@@ -1,19 +1,14 @@
-#PART 1 -IMPORTING TOOLS
-# importing tools from flask,
-# render_templates allows to SHOW HTML PAGE,
-# request is for input from user 
-# REDIRECT is to go to another page
-#Bring in database tool (SQLite)
+# PART 1 - IMPORTING TOOLS
 from flask import Flask, render_template, request, redirect
 import sqlite3
+from datetime import datetime, date
+
 app = Flask(__name__)
-#PART 2 - DATABASE FUNCTIONS
-#create database structure
-#connect to database
-#create table if it doesn't exist
-def init_db():#initialise database
-    conn = sqlite3.connect('database.db')#connect to database
-    cursor = conn.cursor()#Prepare tool to send SQL commands
+
+# PART 2 - DATABASE FUNCTIONS
+def init_db():  # initialise database
+    conn = sqlite3.connect('database.db')  # connect to database
+    cursor = conn.cursor()  # prepare tool to send SQL commands
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS coursework (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -24,9 +19,23 @@ def init_db():#initialise database
     ''')
     conn.commit()
     conn.close()
-#PART 3 - DISPLAY SYSTEM
-#Fetch all coursework from database and display on homepage
-#also sort by due date (ASC = earliest first)
+
+
+# helper function to calculate days left
+def calculate_days_left(due_date_str):
+    due_date_obj = datetime.strptime(due_date_str, "%Y-%m-%d").date()
+    today = date.today()
+    difference = (due_date_obj - today).days
+
+    if difference > 0:
+        return f"{difference} day(s) left"
+    elif difference == 0:
+        return "Due today"
+    else:
+        return f"Overdue by {abs(difference)} day(s)"
+
+
+# PART 3 - DISPLAY SYSTEM
 @app.route('/')
 def home():
     conn = sqlite3.connect('database.db')
@@ -35,8 +44,16 @@ def home():
     coursework_list = cursor.fetchall()
     conn.close()
 
-    return render_template('index.html', coursework_list=coursework_list)
-#PART 4 - ADDING, DELETING AND EDITING COURSEWORK
+    # create new list with extra days_left value
+    coursework_with_days = []
+    for item in coursework_list:
+        days_left = calculate_days_left(item[3])
+        coursework_with_days.append((item[0], item[1], item[2], item[3], days_left))
+
+    return render_template('index.html', coursework_list=coursework_with_days)
+
+
+# PART 4 - ADDING, DELETING AND EDITING COURSEWORK
 @app.route('/add', methods=['POST'])
 def add_coursework():
     title = request.form['title']
@@ -53,17 +70,18 @@ def add_coursework():
     conn.close()
 
     return redirect('/')
+
+
 @app.route('/delete/<int:id>')
 def delete_coursework(id):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-
     cursor.execute("DELETE FROM coursework WHERE id = ?", (id,))
-
     conn.commit()
     conn.close()
 
     return redirect('/')
+
 
 @app.route('/edit/<int:id>')
 def edit_coursework(id):
@@ -74,6 +92,7 @@ def edit_coursework(id):
     conn.close()
 
     return render_template('edit.html', coursework=coursework)
+
 
 @app.route('/update/<int:id>', methods=['POST'])
 def update_coursework(id):
@@ -92,6 +111,8 @@ def update_coursework(id):
     conn.close()
 
     return redirect('/')
+
+
 if __name__ == '__main__':
     init_db()
     app.run(debug=True)
